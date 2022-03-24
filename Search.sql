77f248e4f277ad87420abd9dfcfaa0024cbb9f89
AlexNewport <71346657+AlexNewport@users.noreply.github.com>
2022-03-24 10:43:14 -0400

    Update Search.sql

--- Search.sql
+++ Search.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 04, 2022 at 10:15 AM
+-- Generation Time: Mar 24, 2022 at 10:36 AM
 -- Server version: 8.0.28-0ubuntu0.20.04.3
 -- PHP Version: 7.4.3
 
@@ -76,8 +76,8 @@ CREATE TABLE `Orders` (
   `employeeId` int NOT NULL,
   `clientId` int NOT NULL,
   `date` date NOT NULL,
-  `quantity` int NOT NULL,
-  `totalPrice` int NOT NULL
+  `quantity` int UNSIGNED NOT NULL,
+  `totalPrice` decimal(15,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -85,20 +85,21 @@ CREATE TABLE `Orders` (
 --
 
 INSERT INTO `Orders` (`orderId`, `productId`, `employeeId`, `clientId`, `date`, `quantity`, `totalPrice`) VALUES
-(785, 455, 223, 1120, '2022-02-02', 75, 501),
-(786, 493, 229, 1178, '2022-02-04', 20, 76),
-(787, 467, 240, 1257, '2022-02-07', 80, 4572),
-(788, 458, 233, 1287, '2022-02-07', 15, 85),
-(789, 476, 239, 1251, '2022-02-08', 100, 1600),
-(790, 480, 224, 1135, '2022-02-08', 50, 600),
-(791, 471, 229, 1121, '2022-02-09', 75, 856),
-(792, 458, 229, 1147, '2022-02-09', 145, 3506),
-(793, 467, 233, 1210, '2022-02-09', 343, 6538),
-(794, 469, 239, 1192, '2022-02-10', 262, 4559),
-(795, 457, 224, 1130, '2022-02-11', 40, 450),
-(796, 471, 229, 1283, '2022-02-11', 54, 586),
-(797, 476, 223, 1233, '2022-02-11', 50, 1400),
-(798, 464, 239, 1174, '2022-02-11', 133, 7945);
+(785, 455, 223, 1120, '2022-02-02', 75, '475.00'),
+(786, 468, 229, 1178, '2022-02-04', 20, '168.00'),
+(787, 467, 240, 1257, '2022-02-07', 80, '263.20'),
+(788, 458, 233, 1287, '2022-02-07', 15, '90.00'),
+(789, 473, 239, 1251, '2022-02-08', 100, '8950.00'),
+(790, 479, 224, 1135, '2022-02-08', 50, '750.00'),
+(791, 471, 229, 1121, '2022-02-09', 76, '150.00'),
+(792, 458, 229, 1147, '2022-02-09', 145, '900.00'),
+(793, 467, 233, 1210, '2022-02-09', 343, '1128.47'),
+(794, 469, 239, 1192, '2022-02-10', 262, '3200.00'),
+(795, 457, 224, 1130, '2022-02-11', 40, '175.00'),
+(796, 471, 229, 1283, '2022-02-11', 54, '113.40'),
+(797, 476, 223, 1233, '2022-02-11', 50, '435.00'),
+(798, 464, 239, 1174, '2022-02-11', 133, '2451.19'),
+(799, 455, 0, 1120, '2022-03-04', 50, '327.50');
 
 -- --------------------------------------------------------
 
@@ -110,6 +111,7 @@ CREATE TABLE `Products` (
   `productId` int NOT NULL,
   `name` varchar(20) NOT NULL,
   `stock` int NOT NULL,
+  `price` decimal(4,2) NOT NULL,
   `discontinued` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -117,46 +119,46 @@ CREATE TABLE `Products` (
 -- Dumping data for table `Products`
 --
 
-INSERT INTO `Products` (`productId`, `name`, `stock`, `discontinued`) VALUES
-(455, 'Bingles', 65, 0),
-(456, 'Bongles', 155, 0),
-(457, 'Doodles', 20, 0),
-(458, 'Jinnies', 70, 0),
-(459, 'Tingles', 50, 0),
-(460, 'Reekies', 95, 0),
-(461, 'Popples', 100, 0),
-(462, 'Roggles', 40, 0),
-(463, 'Spitts', 400, 0),
-(464, 'Touruples', 45, 0),
-(465, 'Caxes', 80, 0),
-(466, 'Yistles', 130, 0),
-(467, 'Lipstins', 175, 0),
-(468, 'Quelps', 192, 0),
-(469, 'Ploops', 142, 0),
-(470, 'Liotels', 550, 0),
-(471, 'Pizwizzes', 90, 0),
-(472, 'Nurtebs', 345, 0),
-(473, 'Miyakiznaks', 30, 0),
-(474, 'Wentinoes', 64, 0),
-(475, 'Bertveliens', 705, 0),
-(476, 'Peuds', 405, 0),
-(477, 'Underluds', 550, 0),
-(478, 'Polferits', 240, 0),
-(479, 'Loopbitskers', 40, 0),
-(480, 'Yastiffs', 0, 1),
-(481, 'Lintivs', 0, 1),
-(482, 'Reyels', 40, 0),
-(483, 'Polotsks', 850, 0),
-(484, 'Reprebbles', 55, 0),
-(485, 'Libniololitas', 152, 0),
-(486, 'Kilps', 0, 1),
-(487, 'Paswaps', 95, 0),
-(488, 'Krinspars', 0, 0),
-(489, 'Feldrintatins', 659, 0),
-(490, 'Grubbles', 0, 1),
-(491, 'Stinshirtins', 120, 0),
-(492, 'Bungles', 6, 0),
-(493, 'Witners', 0, 1);
+INSERT INTO `Products` (`productId`, `name`, `stock`, `price`, `discontinued`) VALUES
+(455, 'Bingles', 65, '6.55', 0),
+(456, 'Bongles', 155, '12.30', 0),
+(457, 'Doodles', 20, '4.65', 0),
+(458, 'Jinnies', 70, '6.60', 0),
+(459, 'Tingles', 50, '5.35', 0),
+(460, 'Reekies', 95, '14.80', 0),
+(461, 'Popples', 100, '8.90', 0),
+(462, 'Roggles', 40, '17.52', 0),
+(463, 'Spitts', 400, '23.45', 0),
+(464, 'Touruples', 45, '18.43', 0),
+(465, 'Caxes', 80, '12.35', 0),
+(466, 'Yistles', 130, '11.74', 0),
+(467, 'Lipstins', 175, '3.29', 0),
+(468, 'Quelps', 192, '8.40', 0),
+(469, 'Ploops', 142, '13.12', 0),
+(470, 'Liotles', 550, '43.20', 0),
+(471, 'Pizwizzes', 90, '2.10', 0),
+(472, 'Nurtebs', 345, '73.40', 0),
+(473, 'Miyakiznaks', 30, '94.00', 0),
+(474, 'Wentinoes', 64, '53.80', 0),
+(475, 'Bertveliens', 705, '17.40', 0),
+(476, 'Peuds', 405, '8.70', 0),
+(477, 'Underluds', 550, '30.68', 0),
+(478, 'Polfers', 240, '24.60', 0),
+(479, 'Loopbitskers', 40, '16.20', 0),
+(480, 'Yastiffs', 0, '0.00', 1),
+(481, 'Lintivs', 0, '0.00', 1),
+(482, 'Reyels', 40, '0.00', 0),
+(483, 'Polotsks', 850, '0.00', 0),
+(484, 'Reprebbles', 55, '0.00', 0),
+(485, 'Libniololitas', 152, '0.00', 0),
+(486, 'Kilps', 0, '0.00', 1),
+(487, 'Paswaps', 95, '0.00', 0),
+(488, 'Krinspars', 0, '0.00', 0),
+(489, 'Feldrintatins', 659, '0.00', 0),
+(490, 'Grubbles', 0, '0.00', 1),
+(491, 'Stinshirtins', 120, '0.00', 0),
+(492, 'Bungles', 6, '0.00', 0),
+(493, 'Witners', 0, '0.00', 1);
 
 --
 -- Indexes for dumped tables
